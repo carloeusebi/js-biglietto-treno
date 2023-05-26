8cf9fee74b50994126811b9ee15d47ac60bb1177
carloeusebi <carloeusebi@gmail.com>
2023-05-26 15:58:05 +0200

    Removed console logs from scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-console.log('JSOK');
-
 // # Costants declaration
 const pricePerKm = 0.21;
 const ageMinor = 18;
@@ -16,9 +14,6 @@ const elementDiscounts = document.getElementById('discounts');
 // # Collecting inputs
 const kilometers = parseInt(prompt('How many km are you traveling?', '300'));
 const passengerAge = parseInt(prompt("What's the passenger's age?", '32'));
-
-console.log("Kilometers: " + kilometers);
-console.log("Passenger's Age: " + passengerAge);
 
 // ? Validation
 let isValid = true;
@@ -50,7 +45,6 @@ if (!isValid){
     } else if (passengerAge > ageSenior){
         discountedPrice = price * discountSenior;
     }
-    console.log(`rice: ${price}€`);
 
     // Printing data
     elementTravelDistance.innerText = kilometers + 'km';
