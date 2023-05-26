72f1a27e34a6399fc3b2bc6288ecc74154149c7a
carloeusebi <carloeusebi@gmail.com>
2023-05-26 14:28:36 +0200

    Added data validation

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,34 @@ const passengerAge = parseInt(prompt("What's the passenger's age?", '32'));
 console.log("Kilometers: " + kilometers);
 console.log("Passenger's Age: " + passengerAge);
 
-// TODO Validation
+// ? Validation
+let isValid = true;
+let error = '';
+
+//check if km is a number > 0
+if (!kilometers){
+    isValid = false;
+    error = "ERROR: Km must be a number greater than 0.";
+}
+
+//check if age is 0 < number > 120
+if (!passengerAge || passengerAge < 1 || passengerAge > 120){
+    isValid = false;
+    error += "\nERROR: You must enter a valid age, age must be a number between 0 and 120."
+}
 
 // # Calculating price
-let price = kilometers * pricePerKm;
+if (!isValid){
+    alert(error);
+}else{
 
-if (passengerAge < ageMinor) {
-    price *= discountMinor;
-} else if (passengerAge > ageSenior){
-    price *= discountSenior;
+    let price = kilometers * pricePerKm;
+    
+    if (passengerAge < ageMinor) {
+        price *= discountMinor;
+    } else if (passengerAge > ageSenior){
+        price *= discountSenior;
+    }
+    price = price.toFixed(2);
+    console.log(`Price: ${price}€`);
 }
-price = price.toFixed(2);
-console.log(`Price: ${price}€`);
