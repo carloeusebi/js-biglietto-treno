c83d628cbd7fdd9b83f4f0aea5232c492a664b35
carloeusebi <carloeusebi@gmail.com>
2023-05-26 14:07:19 +0200

    Collected user's inputs

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ const ageMinor = 18;
 const ageSenior = 65;
 const discountMinor = 20;
 const discountSenior = 65;
+
+// # Collecting inputs
+const kilometers = parseInt(prompt('How many km are you traveling?', '300'));
+const passengerAge = parseInt(prompt("What's the passenger's age?", '32'));
+
+console.log("Kilometers: " + kilometers);
+console.log("Passenger's Age: " + passengerAge);
